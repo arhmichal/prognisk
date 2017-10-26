73b0e4955ef33527fc0f1be4137dab5b80e3d293
ARH <hakerti@gmail.com>
2017-10-26 05:28:26 +0200

    prepare blanc cw03

--- cw03/zad_03/excercise.asm
+++ cw03/zad_03/excercise.asm
@@ -1,16 +1,9 @@
-%define NL 10
+%include "lib_arh.macro.asm"
+%include "fuck_io.macro.asm"
 
 section .data   ; Initialized data
 
-    f_ln                    db      NL, 0
-    f_int                   db      "%li", 0
-    f_int_ln                db      "%li", NL, 0
-    f_str                   db      "%s", 0
-    f_str_ln                db      "%s", NL, 0
-
 section .bss    ; UnInitialized data
-
-section .data   ; arrays
 
 section .text   ; the code parto of file
 
@@ -20,98 +13,6 @@ section .text   ; the code parto of file
 ; Napisać funkcję przekształcającą dany łańcuch tekstowy (zakończony zerem) na liczbę całkowitą oraz drugą funkcję przekształcającą liczbę całkowitą (ze znakiem) na łańcuch tekstowy zakończony zerem ( np. dla liczby 123 wynikiem powinno być "123",0). 
 ; Program powinien prezentować możliwości tych funkcji.
 
-extern  printf  ; the C function, to be called
-extern  scanf   ; the C function, to be called
-
-; macros - cause they are like functions, easier, convinient
-;          AND CAN BE OVERLOADED !!!!
-
-%macro sys_exit 0           ; no longer used because of using lib_io64
-    mov     rax, 60         ; sys_exit()
-    syscall                 ; call;
-%endmacro
-
-%macro print 1
-    mov     rdi, %1
-    mov     rax, 0
-    call    printf
-%endmacro
-
-%macro print 2
-    mov     rdi, %1
-    mov     rsi, %2
-    mov     rax, 0
-    call    printf
-%endmacro
-
-%macro print 3
-    mov     rdi, %1
-    mov     rsi, %2
-    mov     rdx, %3
-    mov     rax, 0
-    call    printf
-%endmacro
-
-%macro scan 2
-    mov     rdi, %1
-    mov     rsi, %2
-    mov     rax, 0
-    call    scanf
-%endmacro
-
-%macro return 0
-    leave
-    ret 0;
-%endmacro
-
-%macro return 1
-    leave
-    ret %1;
-%endmacro
-
-; global _start   ; makes it public
-; _start:         ; the main() of assembler ; no longer used because of using lib_io64
-
-global asm_main
-asm_main:
-    enter 0,0
-    call zeroReg
-void_main:
-
+  global asm_main
+function asm_main
     return;
-
-
-
-
-
-; support functions
-
-zeroReg:
-    mov     rax, 0
-    mov     rbx, 0
-    mov     rcx, 0
-    mov     rdx, 0
-    mov     rdi, 0
-    mov     rsi, 0
-    ret;
-
-pushReg:
-    enter   0,0
-    push    rax
-    push    rbx
-    push    rcx
-    push    rdx
-    push    rdi
-    push    rsi
-    return;
-
-popReg:
-    enter   0,0
-    pop     rsi
-    pop     rdi
-    pop     rdx
-    pop     rcx
-    pop     rbx
-    pop     rax
-    return;
-
